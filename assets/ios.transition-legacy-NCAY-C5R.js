0e96277eb3117dee51905cf668922d644f4c171f
Niwresh <sherwinlumakang8272@gmail.com>
2025-03-13 13:59:04 +0800

    Updates

--- assets/ios.transition-legacy-NCAY-C5R.js
+++ assets/ios.transition-legacy-NCAY-C5R.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BMgBw19T.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BTIyUVMI.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
