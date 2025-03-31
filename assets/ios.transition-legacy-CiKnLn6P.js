fb9733f2b50f89fe338e2ffae9fe221bd1d0a9a9
Niwresh <sherwinlumakang827@gmail.com>
2025-03-31 20:37:44 +0800

    Updates

--- assets/ios.transition-legacy-CiKnLn6P.js
+++ assets/ios.transition-legacy-CiKnLn6P.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BTIyUVMI.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BMW8kZ1X.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
